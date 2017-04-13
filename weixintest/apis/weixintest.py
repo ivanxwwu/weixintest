74c7fe0488a2851d952da7bb0eea6054d6491f35
ivan <ivanlife55@163.com>
2017-04-13 15:03:57 +0800

    first

--- weixintest/apis/weixintest.py
+++ weixintest/apis/weixintest.py
@@ -11,12 +11,15 @@ import hashlib
 from ..core import success_response, normal_success_response
 from ..core import config
 from decorators import check_weixin_sig
+from ..models import receivce
+from ..models import reply
 import time
 try:
     import xml.etree.cElementTree as ET
 except ImportError:
     import xml.etree.ElementTree as ET
 import sys
+from ..models import reply
 
 app = flask.Blueprint('weixintest', __name__)
 
@@ -25,13 +28,12 @@ app = flask.Blueprint('weixintest', __name__)
 @check_weixin_sig
 def index():
     #echostr = flask.request.args.get('echostr', '')
-    tree = ET.fromstring(flask.request.data)
-    tousername = tree.find('ToUserName')
-    fromusername = tree.find('FromUserName')
-    create_time = tree.find('CreateTime')
-    content = tree.find('Content')
-    msgid = tree.find('MsgId')
-    msgtype = tree.find('MsgType')
-    fromusername.text, tousername.text = tousername.text, fromusername.text
-    create_time.text = str(int(time.time()))
-    return normal_success_response(ET.tostring(tree))
+    msg = receivce.parse_xml(flask.request.data)
+    if isinstance(msg, receivce.Msg) and msg.MsgType == 'text':
+        touser = msg.FromUserName
+        fromuser = msg.ToUserName
+        content = 'test'
+        replymsg = reply.TextMsg(touser, fromuser, content)
+        return normal_success_response(replymsg.send())
+    else:
+        return normal_success_response("success")
