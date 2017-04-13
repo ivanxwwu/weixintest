621beb969a3ffe0586789791633bf546a1839166
ivan <ivanlife55@163.com>
2017-04-13 13:05:33 +0800

    parse xml

--- weixintest/apis/weixintest.py
+++ weixintest/apis/weixintest.py
@@ -11,12 +11,21 @@ import hashlib
 from ..core import success_response, normal_success_response
 from ..core import config
 from decorators import check_weixin_sig
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+import sys
 
 app = flask.Blueprint('weixintest', __name__)
 
 
 @app.route('/', strict_slashes=False, methods=['GET', 'POST'])
-@check_weixin_sig
+#@check_weixin_sig
 def index():
     echostr = flask.request.args.get('echostr', '')
-    return normal_success_response(echostr)
+    tree = ET.parse(flask.request.data)
+    root = tree.getroot()
+    tousername = root.find('ToUserName')
+    print tousername
+    return normal_success_response("1")
